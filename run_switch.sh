580ca9f4dcce3eca5d3eb8715dc72facf7352433
Javier Munoz <javier@greicodex.com>
2024-04-01 09:18:30 -0400

    Initial commit

---
+++ run_switch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+fs_image=$(docker image ls --filter 'reference=greicodex/freeswitch' -q)
+if [ -z $fs_image ]; then
+    docker build -f docker/Dockerfile . -t greicodex/freeswitch
+fi
+
+CONTROL_PORT=8021
+SIP_INTERNAL=5060
+SIP_INT_TLS=5061
+SIP_EXTERNAL=5080
+SIP_EXT_TLS=5081
+SIP_WS=5066
+SIP_WSS=7443
+VERTO_WS=8081
+VERTO_WSS=8082
+docker run --rm -it \
+    -v ./config:/etc/freeswitch \
+    -v ./sounds:/usr/share/freeswitch/sounds \
+    -v ./tmp:/tmp \
+    -p $CONTROL_PORT:8021  \
+    -p $SIP_INTERNAL:5060  \
+    -p $SIP_INT_TLS:5061  \
+    -p $SIP_EXTERNAL:5080  \
+    -p $SIP_EXT_TLS:5081  \
+    -p $SIP_WS:5066  \
+    -p $SIP_WSS:7443  \
+    -p $VERTO_WS:8081  \
+    -p $VERTO_WSS:8082  \
+    -p 16384-16768:16384-16768/udp \
+     --name fsw1 greicodex/freeswitch  
